e21c8e5c96d376867a682cfbe6580a1121986512
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-06-14 15:34:25 -0600

    Added: Title bar, Password button, Funko Pops background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Pop-logo.png';
+import WonderWoman from './WonderWoman.jpg';
+import TheFlash from './TheFlash.jpg';
+import HarleyQuinn from './HarleyQuinn.jpg';
+import TheJoker from './TheJoker.jpg';
+import GreenArrow from './GreenArrow.jpg';
+import Superman from './Superman.jpg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +14,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Funko for SSH</h1>
+          <h1 className="SSHTextLogo">
+            <p>>_SSH</p>
+            <p>SNEAKER SIDE HUSTLE</p>
+          </h1>
+          <img src={logo} className="Pop-Logo" alt="logo"/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <header className="Password">
+          <img src={WonderWoman} className="WonderWoman" alt="WonderWoman"/>
+          <img src={TheFlash} className="TheFlash" alt="TheFlash"/>
+          <img src={HarleyQuinn} className="HarleyQuinn" alt="HarleyQuinn"/>
+          <img src={TheJoker} className="TheJoker" alt="TheJoker"/>
+          <img src={GreenArrow} className="GreenArrow" alt="GreenArrow"/>
+          <img src={Superman} className="Superman" alt="Superman"/>
+          <button className="button1" onclick="Password()">Enter Site With Password</button>
+        </header>
+
       </div>
     );
   }
